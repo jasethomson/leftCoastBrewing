6a564ab31fc7cdb76236dc027a490eb6446ee284
jasethomson <jasethomson31@gmail.com>
2019-12-23 21:01:12 -0800

    added ul with lis, formatting on hover, and fixed navbar positioning. Also added font-family Open Sans

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -8,15 +8,15 @@ function Navigation(props){
       <div id="logoContainer">
         <img src="/assets/logo.jpg" alt="logo"/>
       </div>
-      <div id="navButtons">
-        <button>About Us</button>
-        <button>Beers</button>
-        <button>Tasting Room</button>
-        <button>Store</button>
-        <button>Beer Club</button>
-        <button>News</button>
-        <button>Contact</button>
-      </div>
+      <ul id="navButtons">
+        <li><a>About Us</a></li>
+        <li><a>Beers</a></li>
+        <li><a>Tasting Room</a></li>
+        <li><a>Store</a></li>
+        <li><a>Beer Club</a></li>
+        <li><a>News</a></li>
+        <li><a>Contact</a></li>
+      </ul>
 
     </div>
   );
