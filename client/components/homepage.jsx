c49b8479d7444fcfc20d7ada3d11d88c29afb604
jasethomson <jasethomson31@gmail.com>
2019-12-24 13:03:45 -0800

    fixed image on about us

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -26,12 +26,14 @@ function Homepage(){
         <div id="homePageAboutUs">
           <img src="/assets/leaveIconLeft.png" alt="left pointing leaf"/>
           <h1>ABOUT US</h1>
-          <img src="/assets/leaveIconLeft.png" alt="left pointing leaf" />
+          <img src="/assets/leaveIconright.png" alt="left pointing leaf" />
         </div>
         <div id="aboutLine"></div>
         <p>Left Coast prides itself on its commitment to quality and constantly strives to find premium ingredients to achieve the perfect balance of aroma and flavor. The family and brewers that comprise Left Coast are passionate about the art and craft of brewing and seek to invent recipes that they not only love drinking but love sharing. Ingredients, technique and passion poured into every pint, that is the Left Coast way.</p>
-        <div id="aboutButton"><button>LEARN MORE ABOUT US</button>
-        </div></div>
+        <div id="aboutButton">
+          <button>LEARN MORE ABOUT US</button>
+        </div>
+      </div>
     </div>
   );
 }
